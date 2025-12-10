bf93fa0443d3a8192bd21f94410b52a2b9f44c1d
Sk Ekram <skekramskekram3@gmail.com>
2025-12-10 12:44:20 +0530

    Enhance Vite config to copy redirects file
    Added a plugin to copy redirects file during build.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { copyFileSync } from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "copy-redirects",
+      closeBundle() {
+        copyFileSync("public/_redirects", "dist/_redirects");
+      }
+    }
+  ],
   server: { port: 5173 },
-})
+});
